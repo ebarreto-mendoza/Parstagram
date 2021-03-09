489edad6f57744aadc609355e8272836d49c72e0
ebarreto-mendoza <60796295+ebarreto-mendoza@users.noreply.github.com>
2021-03-09 00:47:53 -0800

    Part 2

--- parstagram/AppDelegate.swift
+++ parstagram/AppDelegate.swift
@@ -10,6 +10,7 @@ import Parse
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
+    
 
 
 
@@ -21,6 +22,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
                     $0.server = "https://parseapi.back4app.com"
             }
             Parse.initialize(with: parseConfig)
+        
+         
         
         return true
     }
